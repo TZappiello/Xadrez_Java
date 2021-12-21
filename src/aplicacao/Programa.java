58ba150051fc637b917f2b87e3741ade5d2b431d
Tzappiello <tzappiello@gmail.com>
2021-12-21 07:57:32 -0300

    Versao 0.2

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,13 @@
+package aplicacao;
+
+import bordajogo.Posicao;
+
+public class Programa {
+
+	public static void main(String[] args) {
+
+		Posicao pos = new Posicao(3, 5);
+		System.out.println(pos);
+	}
+
+}
---
+++ src/bordajogo/Posicao.java
@@ -0,0 +1,36 @@
+package bordajogo;
+
+public class Posicao {
+	
+	private int linha;
+	private int coluna;
+
+	public Posicao(int linha, int coluna) {
+		this.linha = linha;
+		this.coluna = coluna;
+	}
+
+	public int getLinha() {
+		return linha;
+	}
+
+	public void setLinha(int linha) {
+		this.linha = linha;
+	}
+
+	public int getColuna() {
+		return coluna;
+	}
+
+	public void setColuna(int coluna) {
+		this.coluna = coluna;
+	}
+
+	@Override
+	public String toString() {
+		return linha + ", " + coluna;
+	}
+	
+	
+
+}
